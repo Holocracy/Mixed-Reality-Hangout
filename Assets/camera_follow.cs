c219e892ca1134a922f1552dd9f9c7546faa8e32
mr0ng <sean.ong@outlook.com>
2017-04-13 15:18:11 -0700

    Audio and Motions Now on Photon
    We sidestep the HoloToolkit sharing and use only photon for all
    communication - including object movements. Currently, there are some
    manual adjustments that need to happen for it to work - so it's still a
    work in progress.

---
+++ Assets/camera_follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_follow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(PhotonNetwork.player.isLocal);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+     
+
+        if (PhotonView.isMine)
+        {
+            this.transform.position = Camera.main.transform.position;
+            this.transform.rotation = Camera.main.transform.rotation;
+        }
+
+
+    }
+}
