9c9f886fee7aa90a96294c9d93b2d4c0d2b0fe40
mr0ng <sean.ong@outlook.com>
2017-06-08 15:27:33 -0700

    Avatar Heights
    All Avatar's feet is now anchord to floor grid
    Added floor grid
    User can place floor grid to match their own floor
    Cleaned up project slghtly

--- Assets/camera_follow.cs
+++ Assets/camera_follow.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class camera_follow : Photon.MonoBehaviour {
+    public GameObject floorGrid;
 
 	// Use this for initialization
 	void Start () {
 
         Debug.Log(PhotonNetwork.player.isLocal);
+        floorGrid = GameObject.Find("FloorGrid");
 		
 	}
 	
@@ -18,10 +20,15 @@ public class camera_follow : Photon.MonoBehaviour {
 
         if (photonView.isMine)
         {
-            this.transform.position = Camera.main.transform.position;
-            this.transform.position += new Vector3(0,-2f,0);
-            //this.transform.rotation = Camera.main.transform.rotation;
+            //Only update the X and Z positons (no height movements, just along the ground plane). Fix y height at floor plane level (that's where the Avatar's feet go)
+            this.transform.position = new Vector3(Camera.main.transform.position.x,floorGrid.transform.position.y, Camera.main.transform.position.z);
+            
+            //allow rotation only along one axis, to prevent awkward pivoting of the avatar when the user looks up and down
             transform.rotation = Quaternion.Euler(0, Camera.main.transform.rotation.eulerAngles.y, 0);
+        }
+        else
+        {
+            this.transform.position = new Vector3(this.transform.position.x, floorGrid.transform.position.y, this.transform.position.z);
         }
 
 
