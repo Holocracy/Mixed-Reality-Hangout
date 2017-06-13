22c6476be65a1d2da9de5bdc20f766057217239c
mr0ng <sean.ong@outlook.com>
2017-06-13 08:15:56 -0700

    Added Practical Analytics, removed 4-person limit
    Added practical analytics.
    Removed 4-person limit (set to 99)

---
+++ Assets/PracticalAnalytics/PracticalAnalytics/PracticalConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Practical.Analytics;
+
+namespace Practical
+{
+	public class PracticalConfig : MonoBehaviour
+	{
+		public string APIKey;
+		private PracticalDLL _practicalDLL;
+
+		// Use this for initialization
+		void Start()
+		{
+			_practicalDLL = GetComponent<PracticalDLL>();
+			_practicalDLL.SetAPIKey(APIKey);
+		}
+
+		void Awake()
+		{
+			DontDestroyOnLoad(transform.gameObject);
+		}
+	}
+}
---
+++ Assets/PracticalAnalytics/PracticalAnalytics/PracticalDLL.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using Practical;
+
+namespace Practical.Analytics
+{
+	public class PracticalDLL : MonoBehaviour
+	{
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern System.IntPtr GetErrorMessage(int errorCode);
+
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern System.IntPtr GetLastErrorMessage();
+		
+		// Records how many long a user is looking at an object. Passes timestamp of report, gameobject name & length of time gazing
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern int Startup(string apiKey);
+
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern int RecordGaze(string uniqueIdentifier, string value);
+
+		// Records what keywords are being used.
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern int RecordKeyword(string uniqueIdentifier, string value, string interactedObject);
+
+		// Records what gestures are being used.
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern int RecordGesture(string uniqueIdentifier, string value, string gestureType, string interactedObject, string holdLength);
+
+		// Records customized state
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern int RecordCustomStat(string uniqueIdentifier, string value, string measurement, string formula, string interactedObject);
+
+		// Records when gaining resources
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern int RecordGain(string uniqueIdentifier, string value);
+
+		// Records when exhausting resources
+		[DllImport("PracticalDLL", CallingConvention = CallingConvention.StdCall)]
+		public static extern int RecordLoss(string uniqueIdentifier, string value);
+
+
+		public void SetAPIKey(string LoadedAPIKey)
+		{
+#if WINDOWS_UWP
+			LoadedAPIKey = LoadedAPIKey.Replace(" ","");
+			int errorCode = Startup(LoadedAPIKey);
+			if (errorCode > 0)
+			{
+				string errorMessage = Marshal.PtrToStringAnsi(GetErrorMessage(errorCode));
+				Debug.Log(errorMessage);
+			}
+#endif
+		}
+
+		public void SendEmail()
+		{
+			string email = "support@practicalvr.com";
+			string subject = MyEscapeURL("Practical Feedback");
+			string body = ""; 
+
+			Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+		}
+
+		string MyEscapeURL(string url)
+		{
+			return WWW.EscapeURL(url).Replace("+", "%20");
+		}
+	}
+}
